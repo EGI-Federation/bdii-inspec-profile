c75df2ed7e8a1555c2f3cc42fb3091dc88108733
Bruce Becker <brucellino@gmail.com>
2018-05-29 18:25:00 +0200

    Done with CIS openldap benchmarks

--- controls/openldap-best-practices.rb
+++ controls/openldap-best-practices.rb
@@ -103,3 +103,70 @@ control 'Protect LDAP Export/Import Files' do
     end
   end
 end
+
+control '1.12 Dedicated System' do
+  impact 0.75
+  title 'Put LDAP on a dedicated system if you can'
+  desc 'In order to reduce the risk of potential vulnerabilities in other\n
+             services jeopardizing the LDAP server, consider installing \n
+             OpenLDAP on a dedicated system, or a server with minimal\n
+             services.\n
+             Discussion: System should be dedicated to running OpenLDAP.\n
+             If possible, only administrative services like SSH and other closely\n
+             related authentication services such as radius should be running\n
+             on the same system.'
+  only_if do
+    false
+  end
+end
+
+control '1.13 Restricted File System Access' do
+  impact 0.75
+  title 'Run openldap in a contained environment'
+  desc 'In case the slapd service is exploited remotely file systems access\n
+             restrictions will prevent the exploit from getting access to system\n
+             files and executables.\n
+             Discussion: Consider running slapd in a chroot environment, or using\n
+             SELinux, or Solaris RBAC, to restrict access to system files for the\n
+             slapd user (typically ldap).'
+  only_if do
+    false
+  end
+end
+
+control '2.1 LDAPv2 Bind' do
+  impact 0.75
+  title 'LDAPv2 Bind should be disabled'
+  desc 'Note the bind_v2 option is not enabled by default.\n
+             It allows the old version 2 bind, and does not enable support\n
+             for the entire LDAPv2 protocol.\n
+             Discussion: Do not allow the older ldapv2 bind request unless\n
+             compatibility with old LDAP clients is absolutely necessary.\n
+             Do NOT specify allow bind_v2 in the slapd.conf file.'
+  ldap_config_files = command('find / -name "slapd.conf" ').stdout.split(/\n/)
+  ldap_config_files.each do |f|
+    describe file(f) do
+      its('content') { should_not match /allow bind_v2/ }
+    end
+  end
+end
+
+control 'RootDN password storage' do
+  impact 0.75
+  title 'Root DN password should not be kept in plain text'
+  desc 'Since the Root Distinguished Name password provides full unrestricted\n
+             administrative access to the ldap data, it needs to be carefully protected\n
+             with a secure salted hash value.\n
+             The slappasswd(8) command may be used to generate the hash.\n
+             Administrators will need to update and may need to share the contents\n
+             of the slapd.conf file, placing the rootdn password in a separate file\n
+             helps protects it from accidental disclosure.\n
+             Discussion: The Root Distinguished Name password must NOT be stored\n
+             directly in the slapd.conf file. It may stored in an alternative database\n
+             or service such as SALS or included from a separate protected file\n
+             using a salted MD5 hash value.\n
+             Such as: rootdn  "cn=Manager,dc=example,,dc=com" rootpw {SMD5}sK9tP8eUpB. . .Cq4gFk='
+  only_if do
+    false
+  end
+end
